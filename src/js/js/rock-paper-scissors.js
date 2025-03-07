c4cd9cf55df331ea6b1596650ee949f88cd8273f
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2025-03-07 22:24:49 +0200

    add-js

--- src/js/js/rock-paper-scissors.js
+++ src/js/js/rock-paper-scissors.js
@@ -9,17 +9,3 @@ let userScore = 0;
 let pcScore = 0;
 let drawScore = 0;
 
-
-function playGame(userChoice) {
-    const choices = ["Камінь","Ножиці","Папір"];
-    const pcButtonElement = choices[Math.floor(Math.random() * choices.length)];
-  
-    if(computerChoice === choices[0]) {
-        pcButtonElement.textContent = "Камінь";
-    } else if(computerChoice === choices[1]) {
-        pcButtonElement.textContent = "Папір";
-    } else if(computerChoice === choices[2]) {
-        pcButtonElement.textContent = "Ножиці";
-    }}
-
-    
