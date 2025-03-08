b3df758d22588fee55d0b90f8858504d4d26ef50
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2025-03-08 14:39:08 +0200

    number-js

---
+++ src/js/number-guess.js
@@ -0,0 +1,20 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+document.querySelector('.number__form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const userGuess = parseInt(document.querySelector('.number__input').value);
+    const resultElement = document.getElementById('result');
+
+    if (userGuess === randomNumber) {
+        resultElement.textContent = `Вітаємо! Ви вгадали число ${randomNumber}.`;
+        resultElement.style.color = 'green';
+        randomNumber = Math.floor(Math.random() * 10) + 1; // Загадуємо нове число для нової гри
+    } else {
+        resultElement.textContent = `Нажаль, ви не вгадали. Число, яке загадав комп'ютер, було ${randomNumber}.`;
+        resultElement.style.color = 'red';
+        randomNumber = Math.floor(Math.random() * 10) + 1; // Загадуємо нове число для нової гри
+    }
+});
+
+
+
