074819b220b5e23e24c5dec0deabe9211dce5348
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2025-03-10 16:51:08 +0200

    fix

--- src/js/rock-paper-scissors.js
+++ src/js/rock-paper-scissors.js
@@ -1,42 +1,11 @@
-//вибір людини
 const rockButtonElement = document.querySelector(".game__button-1");
 const  scissorsButtonElement = document.querySelector(".game__button-2");
 const  paperButtonElement = document.querySelector(".game__button-3");
-// кнопка вибору пк
-const pcButtonElement = document.querySelector(".game__pc-option"); //pc
-//рахунок
-const totalScoreElement = document.querySelector(".game__point-all");
-const pcScoreElement = document.querySelector(".game__point-pc");
-const yourScoreElement = document.querySelector(".game__point-your");
-
-const StatusElement = document.querySelector(".game__win-lose")
+const pcButtonElement = document.querySelector(".game__pc-option");
 
 const choices = ["Камінь","Ножиці","Папір"];
 
 let userScore = 0;
 let pcScore = 0;
-let totalScore = 0;
-const loseMessage = "Комп’ютер виграв раунд!";
-const winMessage = "Ви виграли раунд!";
-
-
-rockButtonElement.addEventListener("click", (event) => {
-    console.log(event.target)
-})
-scissorsButtonElement.addEventListener("click", (event) => {
-    console.log(event.target)
-})
-paperButtonElement.addEventListener("click", (event) => {
-    console.log(event.target)
-})
-
-
-pcButtonElement.addEventListener("click", (event) => {
-
-})
-
-totalScoreElement.textContent = totalScore;
-yourScoreElement.textContent = userScore;
-pcScoreElement.textContent = pcScore;
-
+let drawScore = 0;
 
