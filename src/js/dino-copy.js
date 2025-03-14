04653958fe16273af32cc69757940166c22ccf6d
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2025-03-14 10:22:47 +0200

    fix

--- src/js/dino-copy.js
+++ src/js/dino-copy.js
@@ -5,23 +5,31 @@ document.addEventListener("keydown", function(event){
     if (event.code === 'Enter') {
         jump();
     }
+    event.preventDefault;
 })
 
-function jump (){
-if(dino.classList != "jump"){
-    dino.classList.add("jump")
-}
-setTimeout(function (){
-    dino.classList.remove("jump")
-}, 300)
-}
 
-let isAlive = setInterval( function () {
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
 
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
-
+function jump() {
+    if (dino.classList != "jump") {
+      dino.classList.add("jump");
+  
+      setTimeout(function () {
+        dino.classList.remove("jump");
+      }, 300);
     }
+  }
 
+
+let isAlive = setInterval ( function() {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    
+    }
 }, 10)
+
+document.addEventListener("keydown", function (event) {
+    jump();
+  });
