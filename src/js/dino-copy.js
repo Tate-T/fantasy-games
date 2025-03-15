77a63bffba06e4a2589ebbf3a6885bde387229f0
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2025-03-15 16:45:15 +0200

    fixxxdino

--- src/js/dino-copy.js
+++ src/js/dino-copy.js
@@ -1,27 +1,66 @@
 const dino = document.querySelector(".dinos");
-const cactus = document.querySelector(".cactus") ;
+const cactus = document.querySelector(".cactus");
+const loseMsg = document.querySelector(".loseMessages");
+const playButtonElement = document.querySelector(".play-button");
+const startPlateElement = document.querySelector(".start-plate");
+const playElement = document.querySelector(".play");
 
-document.addEventListener("keydown", function(event){
-    if (event.code === 'Enter') {
-        jump();
+let gameStarted = false; 
+
+playButtonElement.addEventListener("click", (event) => {
+    startPlateElement.style.display = "none"; 
+    gameStarted = true; 
+    startGame();
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.code === 'Enter' && !gameStarted) {
+        startPlateElement.style.display = "none";
+        gameStarted = true; 
+        startGame();
     }
-})
+});
 
-function jump (){
-if(dino.classList != "jump"){
-    dino.classList.add("jump")
+function startGame() {
+    loseMsg.style.display = "none"; 
+
+    document.addEventListener("keydown", function(event) {
+        if (event.code === 'Enter') {
+            jump();
+        }
+    });
+
+    let isAlive = setInterval(function() {
+        if (!gameStarted) return;
+
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+        if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+            loseMsg.textContent = "Ви програли!"; 
+            loseMsg.style.display = "block";
+            startPlateElement.appendChild(loseMsg); 
+            gameStarted = false; 
+            clearInterval(isAlive); 
+            resetGame(); 
+        }
+    }, 10);
 }
-setTimeout(function (){
-    dino.classList.remove("jump")
-}, 300)
-}
 
-let isAlive = setInterval( function () {
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
 
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
-
+        setTimeout(function() {
+            dino.classList.remove("jump");
+        }, 300);
     }
+}
 
-}, 10)
+function resetGame() {
+    setTimeout(() => {
+        startPlateElement.style.display = "block"; 
+        loseMsg.style.display = "block";
+        gameStarted = false; 
+    }, 100);
+}
