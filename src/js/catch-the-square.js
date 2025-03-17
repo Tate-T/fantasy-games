a127d65440752505799936c9994098bec5df8b26
rubakhaanastasia <145857108+rubakhaanastasia@users.noreply.github.com>
2025-03-17 17:47:30 +0100

    fix

--- src/js/catch-the-square.js
+++ src/js/catch-the-square.js
@@ -1,13 +1,16 @@
 const square = document.querySelector(".square");
 const scoreDisplay = document.querySelector(".scuare");
+const container = document.querySelector(".catch__the__square");
 let score = 0;
 
 function moveSquare() {
-  const x = Math.random() * (window.innerWidth - 50);
-  const y = Math.random() * (window.innerHeight - 50);
+  const containerRect = container.getBoundingClientRect();
+  const x = Math.random() * (containerRect.width - 50);
+  const y = Math.random() * (containerRect.height - 50);
   square.style.left = `${x}px`;
   square.style.top = `${y}px`;
 }
+
 square.addEventListener("click", () => {
   score++;
   scoreDisplay.textContent = `очки - ${score}`;
